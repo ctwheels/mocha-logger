37ae4a35d71a7bfc4b8867a3f8a1a0ff8d0b1880
ctwheels <ctwheels@live.ca>
2021-01-27 16:53:57 -0500

    Added and updated styling, added more log types, added depth property for log output
    Added warn/warning and info
    Updated styling for items and added color map for Unicode color styling
    Added ability to set console log depth (prefixed spaces) for deep nested output (e.g. multiple describe)

--- index.js
+++ index.js
@@ -1,75 +1,182 @@
-var mcoloring = require('mocha').reporters.Base.color;
-
 /**
-* Convert a JavaScript arguments object into an array and optionally surround it with the given values
-* @param arguments arguments A JavaScript arguments object
-* @param string prefix Optional prefix to apply
-* @param string suffix Optional suffix to apply
-* @return array A standard array of the extracted arguments
+* Log to the console with style
+* @param obj caller (this)
+* @param argObj arguments
+* @param style style to apply
+* @param level level of the console log
 */
-function _argArray(argObj, prefix, suffix) {
-	var args = Array.prototype.slice.call(argObj);
-	if (prefix) args.unshift(prefix);
-	if (suffix) args.push(suffix);
-	return args;
+function apply(obj, argObj, style, level) {
+    var args = Array.prototype.slice.call(argObj);
+	if (style.prefix) {
+        let prefix = style.prefix.repeat(level) + style.icon
+        args.unshift(prefix)
+    }
+	if (style.suffix) args.push(style.suffix);
+	console.log.apply(obj, 
+		args.map((v, k) => k === 0 ? v : colorize(style.color, v)
+	));
 }
 
+/**
+ * Unicode colors map
+ */
+const colors = {
+    black: {
+        light: 90,
+        dark: 30
+    },
+    red: {
+        light: 91,
+        dark: 31
+    },
+    green: {
+        light: 92,
+        dark: 32
+    },
+    yellow: {
+        light: 93,
+        dark: 33
+    },
+    blue: {
+        light: 94,
+        dark: 34
+    },
+    purple: {
+        light: 95,
+        dark: 35
+    },
+    cyan: {
+        light: 96,
+        dark: 36
+    },
+}
+/**
+ * 
+ * @param type Unicode color
+ * @param str string to apply the color to
+ */
+var colorize = function(type, str) {
+  return '\u001b[' + type + 'm' + str + '\u001b[0m';
+}
 
+/**
+ * styles map
+ * sets the text color, prefix, icon, suffix for each console output type
+ */
 var styles = {
 	log: {
-		color: 'error stack', // I know this makes no sence, it just looks better
-		prefix: mcoloring('error stack', '    -'),
+		color: colors.black.light,
+		prefix: '  ',
+        icon: colorize(colors.black.light, '-'),
+		suffix: null,
+    },
+    info: {
+        color: colors.blue.dark,
+        prefix: '  ',
+        icon: colorize(colors.blue.light, '➤'),
+        suffix: null,
+    },
+	pending: {
+		color: colors.cyan.dark,
+		prefix: '  ',
+        icon: colorize(colors.cyan.light, '⧗'),
 		suffix: null,
 	},
-	pending: {
-		color: 'error stack', // I know this makes no sence, it just looks better
-		prefix: mcoloring('error stack', '    -'),
+	warning: {
+		color: colors.yellow.dark,
+		prefix: '  ',
+        icon: colorize(colors.yellow.light,'⚠'),
 		suffix: null,
 	},
 	success: {
-		color: 'pass',
-		prefix: mcoloring('bright pass', '    ✔'),
+		color: colors.green.dark,
+		prefix: '  ',
+        icon: colorize(colors.green.light, '✔'),
 		suffix: null,
 	},
 	error: {
-		color: 'fail',
-		prefix: mcoloring('fail', '    ✖'),
+		color: colors.red.dark,
+		prefix: '  ',
+        icon: colorize(colors.red.light, '✖'),
 		suffix: null,
 	},
 };
 
-function log() {
-	console.log.apply(this, 
-		_argArray(arguments, styles.log.prefix, styles.log.suffix)
-			.map(function(v, k) { return k == 0 ? v : mcoloring(styles.log.color, v) }
-	));
+/**
+ * Logger class
+ */
+class Logger {
+    constructor(level=2) {
+        this.level=level
+        return this
+    }
+    
+    /**
+     * Specifies the depth of the console log line
+     */
+    static level(n) {
+        return new Logger(n)
+    }
+    
+    //#region log
+    log() {
+        apply(this, arguments, styles.log, this.level)
+    }
+    static log() {
+        return new Logger().log(...arguments)
+    }
+    //#endregion
+    
+    //#region info
+    info() {
+        apply(this, arguments, styles.info, this.level)
+    }
+    static info() {
+        return new Logger().info(...arguments)
+    }
+    //#endregion
+
+    //#region pending
+    pending() {
+        apply(this, arguments, styles.pending, this.level)
+    }
+    static pending() {
+        return new Logger().pending(...arguments)
+    }
+    //#endregion
+
+    //#region success
+    success() {
+        apply(this, arguments, styles.success, this.level)
+    }
+    static success() {
+        return new Logger().success(...arguments)
+    }
+    //#endregion
+
+    //#region warn
+    warn() {
+        apply(this, arguments, styles.warning, this.level)
+    }
+    static warn() {
+        return new Logger().warn(...arguments)
+    }
+    warning() {
+        apply(this, arguments, styles.warning, this.level)
+    }
+    static warning() {
+        return new Logger().warning(...arguments)
+    }
+    //#endregion
+
+    //#region error
+    error() {
+        apply(this, arguments, styles.error, this.level)
+    }
+    static error() {
+        return new Logger().error(...arguments)
+    }
+    //#endregion
 }
 
-function pending() {
-	console.log.apply(this, 
-		_argArray(arguments, styles.pending.prefix, styles.pending.suffix)
-			.map(function(v, k) { return k == 0 ? v : mcoloring(styles.pending.color, v) }
-	));
-}
-
-function success() {
-	console.log.apply(this, 
-		_argArray(arguments, styles.success.prefix, styles.success.suffix)
-			.map(function(v, k) { return k == 0 ? v : mcoloring(styles.success.color, v) }
-	));
-}
-
-function error() {
-	console.log.apply(this, 
-		_argArray(arguments, styles.error.prefix, styles.error.suffix)
-			.map(function(v, k) { return k == 0 ? v : mcoloring(styles.error.color, v) }
-	));
-}
-
-module.exports = {
-	styles: styles,
-	log: log,
-	pending: pending,
-	success: success,
-	error: error,
-};
+module.exports = Logger
--- test/simple.js
+++ test/simple.js
@@ -1,6 +1,10 @@
 var mlog = require('..');
 
 mlog.log('This is .log()');
+mlog.info('This is .info()');
 mlog.pending('This is .pending()');
 mlog.success('This is .success()');
+mlog.warn('This is .warn()'); //.warning is also an alias
 mlog.error('This is .error()');
+
+mlog.level(3).log('This is a deep .log()')
